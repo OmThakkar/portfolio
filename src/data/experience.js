943613b2df5f26ad48a388e4f20cea041b6f4524
Om <omthakkar2021.comp@mmcoe.edu.in>
2024-11-20 19:25:14 +0530

    Fix: Updated About Page

--- src/data/experience.js
+++ src/data/experience.js
@@ -8,33 +8,33 @@ const experienceData = [
     img: Ivc,
     tenure: "Oct 2024 - Present",
     title: "Software Development Intern at IVC Ventures Pvt. Ltd.",
-    description: "Worked on various UI/UX projects and improved overall design systems.",
+    description: "At IVC Ventures, I am contributing to the development of a SaaS product by designing scalable systems and interfaces. I’m also involved in development and database management.",
     achievements: [
-      "Designed template UIs and design systems in Figma.",
-      "Collaborated with backend teams to optimize APIs.",
-      "Implemented responsive design for better user experience."
+      "Designed SaaS product interfaces using React and Tailwind CSS component libraries.",
+      "Managed the MongoDB database and collaborated on back-end development.",
+      "Integrated React, Node.js, and Tailwind CSS to build robust, user-friendly systems."
     ]
   },
     {
       img: Harpa,
       tenure: "Feb 2024 - Aug 2024",
       title: "Software Development Intern at Harpa",
-      description: "Worked on various UI/UX projects and improved overall design systems.",
+      description: "As a Software Developer Intern, I contributed to the creation of two major platforms: Favv, a testimonial collection platform, and MyVenue, a banquet management system. ",
       achievements: [
-        "Designed template UIs and design systems in Figma.",
-        "Collaborated with backend teams to optimize APIs.",
-        "Implemented responsive design for better user experience."
+        "Designed Favv, a testimonial platform, from scratch using Figma and developed HTML templates in PHP.",
+        "Created an engaging website for My Venue to showcase its unique features and services.",
+        "Developed additional UI/UX interfaces for client applications and websites."
       ]
     },
     {
       img: EquationsWork,
       tenure: "Dec 2023 - Jan 2024",
       title: "Trainee Software Engineer at Equations Work",
-      description: "Software Trainee Intern at Equations Work",
+      description: "At Equation Works, I worked as a UI/UX Intern, designing interfaces for an in-house SaaS product while contributing to research and documentation. ",
       achievements: [
-        "Developed wireframes and prototypes using Adobe XD.",
-        "Improved accessibility for web apps.",
-        "Managed a team of junior designers."
+        "Designed interfaces using Figma, following UI/UX principles, for an in-house SaaS product.",
+        "Conducted research on front-end technologies for product development.",
+        "Participated in daily scrum meetings and contributed to SRS documentation with the team."
       ]
     }
   ];
